1233eb960e958aecbd6f65e2f69322ed9d92a78f
maedsop <martin.solis06@inacapmail.cl>
2022-05-16 10:15:05 -0400

    Container Añadido

---
+++ SolucionOsiliaSolis/PruebaOsiliaSolis/container.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaOsiliaSolis
+{
+    internal class container
+    {
+        private string codigo;
+        private string marca;
+        private int capacidadMaxima;
+        private byte tamaño;
+        private bool esRefrigerado;
+        private int pesoActual;
+        public Buque buque;
+
+
+
+    }
+}

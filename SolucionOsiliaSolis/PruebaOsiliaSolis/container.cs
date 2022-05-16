f89a3318dbcc7ee2845dd82eb2e28b630662eba6
maedsop <martin.solis06@inacapmail.cl>
2022-05-16 10:42:57 -0400

    Container finalizado

--- SolucionOsiliaSolis/PruebaOsiliaSolis/container.cs
+++ SolucionOsiliaSolis/PruebaOsiliaSolis/container.cs
@@ -11,12 +11,31 @@ namespace PruebaOsiliaSolis
         private string codigo;
         private string marca;
         private int capacidadMaxima;
-        private byte tamaño;
+        private byte tamano;
         private bool esRefrigerado;
         private int pesoActual;
         public Buque buque;
 
+        public string Codigo { get => codigo; }
+        public string Marca { get => marca; set => marca = value;  }
+        public int CapacidadMaxima { get => capacidadMaxima; set => capacidadMaxima = value; }
+        public byte Tamano{ get => tamano; set => tamano = value; }
+        public bool EsRefrigerado { get => esRefrigerado;}
+        public int PesoActual { get => pesoActual; set => pesoActual = value; }
 
+        public container(string codigocontainer)
+        {
+            this.codigo = codigocontainer;
+        }
 
+        public container(string codigo, string marca, int capacidadMaxima, byte tamano, bool esRefrigerado, int pesoActual, Buque buque) : this(codigo)
+        {
+            this.marca = marca;
+            this.capacidadMaxima = capacidadMaxima;
+            this.tamano = tamano;
+            this.esRefrigerado = esRefrigerado;
+            this.pesoActual = pesoActual;
+            this.buque = buque;
+        }
     }
 }
